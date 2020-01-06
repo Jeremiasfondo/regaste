535e5a1b3c5a9d35c16077443384fe455f359887
jerry <jeremiasfondo@gmail.com>
2020-01-06 16:05:16 +0200

    db movie

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import category,movie
+
+admin.site.register(category)
+admin.site.register(movie)
+
+
+
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class category(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "{} - {} ".format(self.title, self.description)
+
+
+
+class movie(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    minutes = models.CharField(max_length=255, null=False)
+    year = models.DateField()
+    main_actor = models.CharField(max_length=255, null=False)
+    productor = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    category   =  models.ForeignKey(category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {}  -  {}".format(self.title, self.year, self.category)
+
+
+
