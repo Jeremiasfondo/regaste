b6ea6749991da35f05aeb08d1383c1d58d5dbb41
jerry <jeremiasfondo@gmail.com>
2020-01-06 16:59:43 +0200

    db payment and trailer

--- movie/admin.py
+++ movie/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 
-from .models import category,movie
+from .models import category,movie,trailer,payment
 
 admin.site.register(category)
+admin.site.register(trailer)
 admin.site.register(movie)
-
-
+admin.site.register(payment)
 
--- movie/models.py
+++ movie/models.py
@@ -9,21 +9,34 @@ class category(models.Model):
     def __str__(self):
         return "{} - {} ".format(self.title, self.description)
 
-
-
-class movie(models.Model):
+class trailer(models.Model):
     title = models.CharField(max_length=255, null=False)
     minutes = models.CharField(max_length=255, null=False)
-    year = models.DateField()
+    release_date  = models.DateField()
     main_actor = models.CharField(max_length=255, null=False)
     productor = models.CharField(max_length=255, null=False)
+    picture = models.ImageField(upload_to = 'templates/images/movie/')
+    url = models.URLField(max_length=225)
     description = models.TextField()
     category   =  models.ForeignKey(category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} - {}  -  {}".format(self.title, self.year, self.category)
+        return "{} - {}  -  {}".format(self.title, self.release_date, self.category)
 
+
+class movie(models.Model):
+    url = models.URLField(max_length=225)
+    trailer   =  models.ForeignKey(trailer, on_delete=models.CASCADE)
+    price = models.CharField(max_length=255, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class payment(models.Model):
+    movie   =  models.ForeignKey(movie, on_delete=models.CASCADE)
+    status = models.BooleanField() 
+    created_date = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
